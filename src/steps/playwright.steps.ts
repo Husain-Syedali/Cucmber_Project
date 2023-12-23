e0043b4a7bd23c39ce2e8ebe0ff72ec7c0eaccaf
Husain-Syedali <husainsoft@gmail.com>
2023-12-23 19:18:22 +0530

    indentation fixes

--- src/steps/playwright.steps.ts
+++ src/steps/playwright.steps.ts
@@ -34,7 +34,7 @@ When('{string} verify menu items', async function (this: ICustomWorld, Item: str
   const page = this.page!;
 
   await page.getByRole('button', { name: Item }).click();
-  if (Item == "Products") {
+  if (Item == 'Products') {
     expect.soft(page.getByRole('link', { name: '/assets/product_coding-' })).toBeVisible();
     await page.getByRole('link', { name: '/assets/product_coding-' }).click();
   } else {
@@ -42,7 +42,7 @@ When('{string} verify menu items', async function (this: ICustomWorld, Item: str
     await page.getByRole('heading', { name: 'By Industry' }).click();
   }
   await page.getByRole('button', { name: Item }).click();
-  if (Item == "Products") {
+  if (Item == 'Products') {
     expect.soft(page.getByRole('link', { name: '/assets/product_integrate-' })).toBeVisible();
     await page.getByRole('link', { name: '/assets/product_integrate-' }).click();
   } else {
@@ -52,19 +52,15 @@ When('{string} verify menu items', async function (this: ICustomWorld, Item: str
 
   await page.getByRole('button', { name: Item }).click();
 
-  if (Item == "Products") {
+  if (Item == 'Products') {
     await page.getByRole('link', { name: '/assets/product_teach-' }).click();
   }
-
-
-
 });
 let icount = false;
 Given('Verify {string} language selection', async function (this: ICustomWorld, language: string) {
 
   const page = this.page!;
   await page.waitForTimeout(2000);
-  //await page.locator('#code div').filter({ hasText: 'public class MyClass { public' }).nth(1).waitFor()
   if (icount == false) {
     icount = true;
     await page.goto(config.BASE_URL);
@@ -80,28 +76,25 @@ Given('Verify {string} language selection', async function (this: ICustomWorld, 
 Given('click on Price tab', async function () {
   const page = this.page!;
   await page.goto('https://www.jdoodle.com/online-java-compiler');
-  await page.getByRole('link', { name: 'Pricing' })
+  await page.getByRole('link', { name: 'Pricing' });
 
   await page.getByRole('link', { name: 'Pricing' }).click();
+
   //Verification
   await page.waitForTimeout(10000);
-
-
-
-
 });
 
 Then('verify Platform Price page', async function () {
-  // Write code here that turns the phrase above into concrete actions
+
   const page = this.page!;
-  await expect.soft(page.getByRole('tabpanel')).toContainText('Team🔥 Most Popular$100/ MonthPower your team\'s successIDE and Plugins Standard');
-  await expect.soft(page.getByRole('tabpanel')).toContainText('Unlock the basics');
-  await expect.soft(page.getByRole('tabpanel')).toContainText('/ Month');
-  await expect.soft(page.getByRole('tabpanel')).toContainText('IDE and Plugins');
-  await expect.soft(page.getByRole('tabpanel')).toContainText('Standard IDE');
-  await expect.soft(page.getByRole('tabpanel')).toContainText('Unlimited customizable WYSIWYG Plugins');
-  await expect.soft(page.getByRole('tabpanel')).toContainText('File upload feature');
-  await expect.soft(page.getByRole('tabpanel')).toContainText('Plugin to host page communication');
+  await expect.soft(page.getByRole('tabpanel')).toContainText("Team🔥 Most Popular$100/ MonthPower your team\'s successIDE and Plugins Standard");
+  await expect.soft(page.getByRole('tabpanel')).toContainText("Unlock the basics");
+  await expect.soft(page.getByRole('tabpanel')).toContainText("/ Month");
+  await expect.soft(page.getByRole('tabpanel')).toContainText("IDE and Plugins");
+  await expect.soft(page.getByRole('tabpanel')).toContainText("Standard IDE");
+  await expect.soft(page.getByRole('tabpanel')).toContainText("Unlimited customizable WYSIWYG Plugins");
+  await expect.soft(page.getByRole('tabpanel')).toContainText("File upload feature");
+  await expect.soft(page.getByRole('tabpanel')).toContainText("Plugin to host page communication");
   await page.waitForTimeout(10000);
 });
 
@@ -111,8 +104,6 @@ Then('verify API Price page', async function () {
   await page.getByRole('tab', { name: 'API' }).click();
   await page.waitForTimeout(10000);
 });
-
-
 
 //await page.getByRole('tab', { name: 'API' }).click();
 
