25cfde414a60182a95aee63ea7a2ee41b7785e7c
Husain-Syedali <husainsoft@gmail.com>
2023-12-23 18:29:12 +0530

    added cases
    added UI cases

--- features/playwright.feature
+++ features/playwright.feature
@@ -1,22 +1,29 @@
 @foo
-Feature: Verify JDoodle UI
+Feature: Verify JDoodle suites
 
   Background: Navigation
     Given JDoodle website
 
-  Scenario: Verify Code editor and clicking execute button
-    Given Code Snippet in editor
-    When Clicked on Execute option
+ Scenario: Verify all hoverable menu items
+    Given "Products" verify menu items
+    Given "Solutions" verify menu items
 
 
- Scenario: Verify menu items
-    Given Products menu items
-
-
- Scenario: Verify menu language dropdown items
+ Scenario: Verify language dropdown menu items
     Then Verify "Java (Advanced) Java (" language selection
     Then Verify "C#" language selection
     Then Verify "Python3" language selection
     Then Verify "C++" language selection
     Then Verify "NodeJS" language selection
     Then Verify "PHP" language selection
+
+@Scenario1
+Scenario: Verify Code pricing section
+    Given click on Price tab
+    Then verify Platform Price page
+    Then verify API Price page
+  
+
+Scenario: Verify Code execution #Currently blocked due to 
+    Given Code Snippet in editor
+    When Clicked on Execute option
--- src/steps/playwright.steps.ts
+++ src/steps/playwright.steps.ts
@@ -1,6 +1,7 @@
 import { ICustomWorld } from '../support/custom-world';
 import { config } from '../support/config';
-import { Given, When } from '@cucumber/cucumber';
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect } from 'playwright/test';
 //import { expect } from '@playwright/test';
 
 Given('JDoodle website', async function (this: ICustomWorld) {
@@ -29,15 +30,33 @@ When('Clicked on Execute option', async function (this: ICustomWorld) {
 
 });
 
-When('Products menu items', async function (this: ICustomWorld) {
+When('{string} verify menu items', async function (this: ICustomWorld, Item: string) {
   const page = this.page!;
 
-  await page.getByRole('button', { name: 'Products' }).click();
-  await page.getByRole('link', { name: '/assets/product_coding-' }).click();
-  await page.getByRole('button', { name: 'Products' }).click();
-  await page.getByRole('link', { name: '/assets/product_integrate-' }).click();
-  await page.getByRole('button', { name: 'Products' }).click();
-  await page.getByRole('link', { name: '/assets/product_teach-' }).click();
+  await page.getByRole('button', { name: Item }).click();
+  if (Item == "Products") {
+    expect.soft(page.getByRole('link', { name: '/assets/product_coding-' })).toBeVisible();
+    await page.getByRole('link', { name: '/assets/product_coding-' }).click();
+  } else {
+    expect.soft(page.getByRole('heading', { name: 'By Industry' })).toBeVisible();
+    await page.getByRole('heading', { name: 'By Industry' }).click();
+  }
+  await page.getByRole('button', { name: Item }).click();
+  if (Item == "Products") {
+    expect.soft(page.getByRole('link', { name: '/assets/product_integrate-' })).toBeVisible();
+    await page.getByRole('link', { name: '/assets/product_integrate-' }).click();
+  } else {
+    expect.soft(page.getByRole('heading', { name: 'Use Cases' })).toBeVisible();
+    await page.getByRole('heading', { name: 'Use Cases' }).click();
+  }
+
+  await page.getByRole('button', { name: Item }).click();
+
+  if (Item == "Products") {
+    await page.getByRole('link', { name: '/assets/product_teach-' }).click();
+  }
+
+
 
 });
 let icount = false
@@ -48,6 +67,7 @@ Given('Verify {string} language selection', async function (this: ICustomWorld, 
   //await page.locator('#code div').filter({ hasText: 'public class MyClass { public' }).nth(1).waitFor()
   if (icount == false) {
     icount = true
+    await page.goto(config.BASE_URL);
     await page.locator('#navbar-collapse-basic div').filter({ hasText: 'Ada' }).nth(4).click();
   } else {
     await page.getByPlaceholder('Search Language').click();
@@ -56,6 +76,45 @@ Given('Verify {string} language selection', async function (this: ICustomWorld, 
   await page.getByRole('link', { name: language }).last().click();
   await page.waitForTimeout(10000);
 })
+
+Given('click on Price tab', async function () {
+  const page = this.page!;
+  await page.goto('https://www.jdoodle.com/online-java-compiler');
+  await page.getByRole('link', { name: 'Pricing' })
+
+  await page.getByRole('link', { name: 'Pricing' }).click();
+  //Verification
+  await page.waitForTimeout(10000);
+
+
+
+
+});
+
+Then('verify Platform Price page', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  const page = this.page!;
+  await expect.soft(page.getByRole('tabpanel')).toContainText('Team🔥 Most Popular$100/ MonthPower your team\'s successIDE and Plugins Standard');
+  await expect.soft(page.getByRole('tabpanel')).toContainText('Unlock the basics')
+  await expect.soft(page.getByRole('tabpanel')).toContainText('/ Month')
+  await expect.soft(page.getByRole('tabpanel')).toContainText('IDE and Plugins')
+  await expect.soft(page.getByRole('tabpanel')).toContainText('Standard IDE')
+  await expect.soft(page.getByRole('tabpanel')).toContainText('Unlimited customizable WYSIWYG Plugins')
+  await expect.soft(page.getByRole('tabpanel')).toContainText('File upload feature')
+  await expect.soft(page.getByRole('tabpanel')).toContainText('Plugin to host page communication')
+  await page.waitForTimeout(10000);
+});
+
+Then('verify API Price page', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  const page = this.page!;
+  await page.getByRole('tab', { name: 'API' }).click();
+  await page.waitForTimeout(10000);
+});
+
+
+
+//await page.getByRole('tab', { name: 'API' }).click();
 
 // Then('We see {string} mode', async function (this: ICustomWorld, mode: string) {
 //   const page = this.page!;
